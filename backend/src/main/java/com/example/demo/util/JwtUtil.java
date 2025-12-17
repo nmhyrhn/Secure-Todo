e6bef86aecf7748081798ed34437dc403e81e395
nmhyrhn <gbs06193@gmail.com>
2025-12-17 18:12:25 +0900

    251217

--- backend/src/main/java/com/example/demo/util/JwtUtil.java
+++ backend/src/main/java/com/example/demo/util/JwtUtil.java
@@ -1,5 +1,7 @@
 package com.example.demo.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,7 @@ public class JwtUtil {
     private final String SECRET = "secure-todo-secret-key";
     private final long EXPIRATION = 1000 * 60 * 60; // 1시간
 
+    // 토큰 생성
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
@@ -19,5 +22,24 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION))
                 .signWith(SignatureAlgorithm.HS256, SECRET)
                 .compact();
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        try{
+            Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    //Claims 추출
+    public Claims getCalims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token).getBody();
     }
 }
